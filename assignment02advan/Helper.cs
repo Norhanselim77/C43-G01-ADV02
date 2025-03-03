55e39693b9d2c60dd6d5ff228e86a9a5d8ad1cd2
Norhan77 <mn2hafsa@gmail.com>
2025-03-03 03:07:28 +0200

    .

---
+++ assignment02advan/Helper.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment02advan
+{
+    internal class Helper
+    {
+        #region 1
+        public static void PrintHowManyNumsGreaterThanX(int[] arr, int X)
+        {
+            int count = 0;
+            foreach (int num in arr)
+            {
+                if (num > X)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+
+        }
+        #endregion
+
+        #region 2
+
+        public static bool IfItIsPalindrome(int[] arr, int X)
+        {
+            for (int i = 0; i < X / 2; i++)
+            {
+                if (arr[i] != arr[X - i - 1])
+                    return false;
+            }
+            return true;
+        }
+        #endregion
+
+        #region 3
+
+        public static void ReverseQueue<T>(Queue<T> queue)
+        {
+            Stack<T> tempStack = new Stack<T>();
+            while (queue.Count > 0)
+            {
+                tempStack.Push(queue.Dequeue());
+            }
+            while (tempStack.Count > 0)
+            {
+                queue.Enqueue(tempStack.Pop());
+            }
+        }
+        #endregion
+
+        #region 4
+        public static bool IsBalanced(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')' || c == ']' || c == '}')
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+                    char top = stack.Pop();
+
+                    if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '['))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        #endregion
+
+        #region 5
+
+        public static void RemoveDuplicates(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        arr[j] = -1;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == -1)
+                {
+                    break;
+                }
+                Console.WriteLine(arr[i]);
+            }
+        }
+        #endregion
+
+        #region 6
+
+        public static void RemoveOddNumbers(ArrayList array)
+        {
+            for (int i = 0; i < array.Count; i++)
+            {
+                if ((int?)array[i] % 2 != 0)
+                {
+                    array.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+        #endregion
+
+        #region 8
+
+        public static void CheckTargetSum<T>(Stack<T> stack, T Target)
+        {
+            foreach (T item in stack)
+            {
+                if (item.Equals(Target))
+                {
+                    Console.WriteLine($"Target was found successfully and the count = {stack.Count - 1}");
+                    return;
+                }
+            }
+            Console.WriteLine("Target was not found");
+        }
+
+        #endregion
+
+        #region 9
+
+        public static int[] GetIntersection(int[] arr1, int[] arr2)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (arr1[i] == arr2[j])
+                    {
+                        list.Add(arr1[i]);
+                        break;
+                    }
+                }
+            }
+            return list.ToArray();
+        }
+
+        #endregion
+
+        #region 10
+
+        public static List<int> TargetSum(ArrayList array, int target)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < array.Count; i++)
+            {
+                for (int j = i + 1; j < array.Count; j++)
+                {
+                    if ((int)array[i] + (int)array[j] == target)
+                    {
+                        list.Add((int)array[i]);
+                        list.Add((int)array[j]);
+                        break;
+                    }
+                }
+            }
+            return list;
+        }
+        #endregion
+
+        #region 11
+        public static void reverseQueueFirstKElements<T>(Queue<T> queue, int k)
+        {
+            if (queue.Count == 0 || k > queue.Count)
+            {
+                return;
+            }
+            if (k <= 0)
+            {
+                return;
+            }
+            Stack<T> stack = new Stack<T>();
+            for (int i = 0; i < k; i++)
+            {
+                stack.Push(queue.Dequeue());
+            }
+            while (stack.Count > 0)
+            {
+                queue.Enqueue(stack.Pop());
+            }
+            for (int i = 0; i < queue.Count - k; i++)
+            {
+                queue.Enqueue(queue.Dequeue());
+            }
+        }
+        #endregion
+    }
+}
---
+++ assignment02advan/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace assignment02advan
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region 1
+
+            //Helper.PrintHowManyNumsGreaterThanX(new int[] { 11, 5, 3 }, 1);
+            //Helper.PrintHowManyNumsGreaterThanX(new int[] { 11, 5, 3 }, 5);
+            //Helper.PrintHowManyNumsGreaterThanX(new int[] { 11, 5, 3 }, 13);
+
+            #endregion
+
+            #region 2
+
+            //Console.WriteLine(Helper.IfItIsPalindrome(new int[] { 1, 3, 2, 3, 1 }, 5) ? "YES" : "NO");
+
+            #endregion
+
+            #region 3
+            //Queue<int> queue = new Queue<int>();
+            //queue.Enqueue(1);
+            //queue.Enqueue(2);
+            //queue.Enqueue(3);
+            //queue.Enqueue(4);
+            //Helper.ReverseQueue(queue);
+            //foreach (int item in queue)
+            //{
+            //    Console.Write($"{item} , ");
+            //}
+            #endregion
+
+            #region 4
+
+            //Console.WriteLine(Helper.IsBalanced("[()]{}") ? "Balanced" : "Not Balanced");
+
+            #endregion
+
+            #region 5
+
+            //int[] arr = new int[] { 1, 2, 3, 4, 2, 3, 4, 5, 6, 1 };
+            //Helper.RemoveDuplicates(arr);
+            ////foreach (int item in arr)
+            ////Console.WriteLine(item);
+            #endregion
+
+            #region 6
+
+            //ArrayList array = new ArrayList();
+            //array.AddRange(new int[] { 1, 2, 3, 4, 5 });
+            //Helper.RemoveOddNumbers(array);
+            //foreach (int item in array)
+            //    Console.WriteLine(item);
+
+            #endregion
+
+            #region 7
+
+            //Queue queue = new Queue();
+            //queue.Enqueue(1);
+            //queue.Enqueue("Apple");
+            //queue.Enqueue(5.28);
+            //foreach (var item in queue)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            #region 8
+
+            //Stack<int> stack = new Stack<int>();
+            //stack.Push(1);
+            //stack.Push(2);
+            //stack.Push(3);
+            //stack.Push(4);
+            //Helper.CheckTargetSum(stack, 4);
+            //Helper.CheckTargetSum(stack, 5);
+            #endregion
+
+            #region 9
+            //int[] array = Helper.GetIntersection(new int[] { 1, 2, 3, 4, 4 }, new int[] { 10, 4, 4 });
+            //foreach (int item in array)
+            //{
+            //    Console.Write($"{item},");
+            //}
+            #endregion
+
+            #region 10
+            //ArrayList array = new ArrayList() { 1, 2, 3, 7, 5 };
+            //Helper.TargetSum(array, 2);
+            #endregion
+
+            #region 11
+            //Queue<int> queue = new Queue<int>();
+            //queue.Enqueue(1);
+            //queue.Enqueue(2);
+            //queue.Enqueue(3);
+            //queue.Enqueue(4);
+            //queue.Enqueue(5);
+            //Helper.reverseQueueFirstKElements(queue, 3);
+            //foreach (int item in queue)
+            //{
+            //    Console.Write($"{item},");
+            //}
+
+            #endregion
+        }
+    }
+}
